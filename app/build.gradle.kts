91300d11f739dcdab548fd720830eae029b76efb
Abinash6000 <abinashdash6000@gmail.com>
2026-06-10 19:10:42 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.project.chantharekrishna"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
